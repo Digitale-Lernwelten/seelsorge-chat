12b584b3b065961d4d1750de41149a4f5828706d
paetzenstein <dev@paetzenstein.com>
2022-04-04 19:31:29 +0200

    fixed js not loading

--- web/assets/main.js
+++ web/assets/main.js
@@ -2,23 +2,22 @@ const serviceTime = () => {
 		const currentDay = new Date().getDay();
 		const today = new Date();
 		const currentTime = (today.getHours() * 100) + today.getMinutes();
-		/* Service Time
 		if (currentDay >= 1 && currentDay <= 5) {
 			if (currentTime >= 1545 && currentTime <= 1930) {
 				return true;
 			}
 			return false;
 		}
-		*/
 		return true;
 }
 
-const toggleBehaviour = () => {
+async function toggleBehaviour() {
+	const freeSlots = await getFreeSlots();
 	const outOfService = document.querySelector(".service-time");
 	const noSlots = document.querySelector(".no-slots");
 	if (serviceTime() === false) {
 		outOfService.classList.add("active");
-	} else if (await getFreeSlots() === 0) {
+	}else if (freeSlots === 0) {
 		noSlots.classList.add("show");
 	}
 }
@@ -78,19 +77,40 @@ window.onscroll = function () {
 		noSlots.classList.remove("offset");
 	}
 }
-
 const getFreeSlots = async () => {
-	const rawResponse = await fetch('/status.php', {
-		cache: "no-store",
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		}
-	});
-	const content = await rawResponse.json();
-	if (content && content.slotsFree) {
-		return content.slotsFree | 0;
-	} else {
-		return 0;
-	}
+    console.time('operator-fetch')
+    const rawResponse = await fetch('https://seelsorge-chat.de/status.php', {
+        cache: "no-store",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    console.timeEnd('operator-fetch');
+    /**
+     * @type {Object}
+     * @property {number} content.slotsFree - number of availabe operators
+     * @property {string | null} content.error - optional error during API call
+     * @property {boolean} content.usingCache - if the result is cached or not
+     * @property {number} content.totalOps - total number of operators
+     * @property {number} content.apiCode - UserLike API reply code
+     * @property {string[]} content.warnings - warnings during API call
+     * @property {number} content.duration - time it took for the API call on the server in seconds
+     */
+    const content = await rawResponse.json();
+    console.log(`script execution took: ${content.duration.toFixed(2)}s`);
+    if (content.usingCache) {
+        console.warn('using cached API results');
+    }
+    content.warnings.forEach(warning => {
+        console.warn(warning);
+    });
+    if (content.error !== null) {
+        console.error(content.error);
+    }
+    if (content && content.slotsFree) {
+        return content.slotsFree | 0;
+    } else {
+        return 0;
+    }
 }
